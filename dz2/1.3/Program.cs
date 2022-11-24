cdb9cf4a6d975997adc6a5d536b911dc78c05ee2
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-11-24 23:54:05 +0300

    3d

---
+++ dz2/1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿string TreriaCifra(int num)
+{
+    if (num >= 100) 
+{
+    while (num > 999)
+    {   
+        num = num / 10;
+    }
+    num = num % 10;
+    return $"{num}";
+}
+else
+{
+    return "ошибка";
+}
+}
+
+Console.WriteLine("Введите число");
+int chis = int.Parse(Console.ReadLine());
+Console.WriteLine(TreriaCifra(chis));
